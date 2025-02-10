113299380049eae5c8273c56e30c26f10750bd41
Caitlyn Kim <cakim@smith.edu>
2025-02-10 08:39:12 -0500

    added comments to computer and oo_resale_shop

--- computer.py
+++ computer.py
@@ -1,7 +1,8 @@
+# creates the attributes and method for each computer
 class Computer:
 
     # What attributes will it need?
-    # should store details about a computer (os system, how much computer costs, refurbishing computer)
+    # saves these details for each computer
     description: str
     processor_type: str
     hard_drive_capacity: int
@@ -12,6 +13,7 @@ class Computer:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
+    # creates a computer object with the attributes from above
     def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int):
         self.description = description
         self.processor_type = processor_type
@@ -23,7 +25,7 @@ class Computer:
         
     # What methods will you need?
     
-    # 
+    # updates a computer's price
     def update_price(self, new_price: int):
         self.price = new_price
 
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,28 +1,33 @@
 from computer import *
-from typing import Optional #
+from typing import Optional # for the optional parameter new_os in refurbish method
 
+# should keep track of store's inventory (list of individual computer instances, buying/selling/updating prices/refurbishing computers)
 class ResaleShop:
 
     # What attributes will it need?
-    # should keep track of store's inventory (list of individual computer instances, buying a computer (adding to inventory), selling a computer (removing from inventory))
+    # where list of computers and their details are stored
     inventory = []
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__(self):
         self.inventory = []
 
     # What methods will you need?
+
+    # adds a new computer to inventory
     def buy(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int):
         new_computer = Computer(description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price)
         self.inventory.append(new_computer)
 
+    # updates price of a particular computer
     def update_price(self, item_id: int, new_price: int):
-        if 0 <= item_id < len(self.inventory): #starting item index at 0, so item index must be less than # of computers in inventory
-            # 
-            self.inventory[item_id].update_price(new_price)
+        if 0 <= item_id < len(self.inventory): #starting item index at 0, so item index must be less than # of computers in inventory 
+            self.inventory[item_id].update_price(new_price) # updates the price of a computer at a particular index in inventory
         else:
             print("Item", item_id, "not found. Cannot update price.")
-
+    
+    # removes a particlar computer in inventory
     def sell(self, item_id: int):
         if 0 <= item_id < len(self.inventory): #starting item index at 0, so item index must be less than # of computers in inventory
             self.inventory.pop(item_id)
@@ -30,6 +35,7 @@ class ResaleShop:
         else: 
             print("Item", item_id, "not found. Please select another item to sell.")
 
+    # prints all computer details in inventory
     def print_inventory(self):
         # If the inventory is not empty
         if self.inventory:
@@ -40,8 +46,9 @@ class ResaleShop:
         else:
             print("No inventory to display.")
     
+    # updates a particular computer's selling price and new operating system (if given)
     def refurbish(self, item_id: int, new_os: Optional[str] = None):
-        if 0 <= item_id < len(self.inventory): #starting item index at 0, so item index must be less than # of computers in inventory
+        if 0 <= item_id < len(self.inventory): # starting item index at 0, so item index must be less than # of computers in inventory
             computer = self.inventory[item_id] # locate the computer
             if int(computer.year_made) < 2000:
                 computer.update_price(0) # too old to sell, donation only
